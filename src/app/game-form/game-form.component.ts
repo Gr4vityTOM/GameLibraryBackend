5194dc3d8ea01e1a632a5f87f64640c62a6b02b9
Michal Húlan <michal.hulan@kosickaakademia.sk>
2023-12-12 23:01:55 +0100

    added method onSubmit() in game-form.component.ts and made some changes in the game-form.component.html

--- src/app/game-form/game-form.component.ts
+++ src/app/game-form/game-form.component.ts
@@ -1,26 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { Game } from '../../entities/game';
 import { StoreService } from '../../services/store.service';
+import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-game-form',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule
+    ],
   templateUrl: './game-form.component.html',
   styleUrl: './game-form.component.css'
 })
 export class GameFormComponent {
 
+
     name: string = ''
     releasedate: number = 0
     genre: string = ''
     developer: string = ''
-  
+  router = inject(Router);
+
+
   constructor(private storeService: StoreService) {}
 
   //Metoda on Submit
-  onSubmit(name: string, releasedate: number, genre: string, developer: string){
-    
+  onSubmit(){
+    this.storeService.addGame(new Game(this.name,this.releasedate,this.genre,this.developer))
+    this.router.navigate(["/store"]);
   }
 };
 
