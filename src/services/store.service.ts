98abedd2f98003b9fc29ba9760564afcf263af26
Tomas <tomas.polasko@kosickaakademia.sk>
2023-12-12 20:02:00 +0100

    Added methods

--- src/services/store.service.ts
+++ src/services/store.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import {Game} from "../entities/game";
+import {HttpClient} from "@angular/common/http";
+import {MessageService} from "./message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
+
+  constructor(private http: HttpClient, private message: MessageService) { }
+
   private games = [
         new Game("Crysis",2007,"FPS","EA"),
         new Game("Minecraft",2011,"Survival","Mojang"),
         new Game("Red dead redemption 2",2018,"Adventure","Rockstar"),
         new Game("Grand Theft Auto 5",2013,"Story","Rockstar")
   ]
-  constructor() { }
+
+  addGame(game:Game){
+    this.games.push(game);
+  }
+
+  getGames():Game[]{
+    return this.games;
+  }
+
 }
