e40f5c98bfadc68151f67316afe644b259b2906d
Michal Húlan <michal.hulan@kosickaakademia.sk>
2023-12-12 22:24:44 +0100

    added game-form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {LoginComponent} from "./login/login.component";
 import {LibraryComponent} from "./library/library.component";
 import {StoreComponent} from "./store/store.component";
+import { GameFormComponent } from './game-form/game-form.component';
 
 export const routes: Routes = [
   {path:"library",component:LibraryComponent},
   {path:"store",component:StoreComponent},
   {path:"login",component:LoginComponent},
+  {path:"game-form", component: GameFormComponent},
   {path:"",redirectTo:"/login",pathMatch: "full"},
   {path:"**",component:PageNotFoundComponent}
+
 ];
--- src/app/game-form/game-form.component.ts
+++ src/app/game-form/game-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Game } from '../../entities/game';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-game-form',
@@ -9,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class GameFormComponent {
 
-}
+    name: string = ''
+    releasedate: number = 0
+    genre: string = ''
+    developer: string = ''
+  
+  constructor(private storeService: StoreService) {}
+
+  //Metoda on Submit
+  onSubmit(name: string, releasedate: number, genre: string, developer: string){
+    
+  }
+};
+
+    
